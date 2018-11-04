a360ac3d5c666649b876aa07a654a351bf1224e2
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-04 00:07:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       :isosceles
     elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
       :scalene
-    elsif @side1 !> 0 || @side2 !> 0 || @side3 !> 0
+    elsif @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       raise TriangleError
     end
   end
